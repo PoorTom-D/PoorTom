9514ec86dc8c32fdf8c89becddbe2da9c1b229a5
lenovo <41428945@qq.com>
2021-03-25 22:22:50 +0800

    the first time

---
+++ homework_theSecondSemesterOfMyJuniorYear/class_01/Test02.java
@@ -0,0 +1,17 @@
+package pay_train_weekind.homework_theSecondSemesterOfMyJuniorYear.class_01;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 将当前系统时间以"yyyy-MM-dd HH:mm:ss"格式输出
+ * @author Bonnie
+ *
+ */
+public class Test02 {
+    public static void main(String[] args) {
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date nowdate=new Date();
+        System.out.println("当前日期:"+sdf.format(nowdate));
+    }
+}
---
+++ homework_theSecondSemesterOfMyJuniorYear/class_01/Test03.java
@@ -0,0 +1,24 @@
+package pay_train_weekind.homework_theSecondSemesterOfMyJuniorYear.class_01;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+/**
+ * 输入某人生日，格式为"yyyy-MM-dd"，输出到现在为止经过了多少周。
+ * @author Bonnie
+ *
+ */
+public class Test03 {
+    public static void main(String[] args) throws ParseException {
+        Scanner reader=new Scanner(System.in);
+        System.out.println("请输入生日(格式为yyyy-MM-dd)：");
+        String birthday=reader.nextLine();
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-mm-dd");
+        Date birthdate=sdf.parse(birthday);
+        Date nowdate=new Date();
+        long weeks=(nowdate.getTime()-birthdate.getTime())/(1000*60*60*24*7);
+        System.out.println("经过了"+weeks+"周");
+    }
+}
